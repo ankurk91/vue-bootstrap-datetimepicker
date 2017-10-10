5eef5ae63bd759f3e7102626250531413afd82b3
ankurk91 <pro.ankurk1@gmail.com>
2017-10-10 17:34:59 +0530

    Add test for event emits

---
+++ __test__/specs/events.spec.js
@@ -0,0 +1,46 @@
+import component from '../../src/component.vue';
+// Lets import full build
+import Vue from 'vue/dist/vue.common';
+
+describe('datepicker component', () => {
+
+  // Make a copy of local vue
+  let localVue = Vue.extend();
+
+  test('emits events', (done) => {
+    let app = localVue.extend({
+      template: `<div id="app">
+                  <date-picker class="date-picker" v-model="date" :config="config" @dp-change="onChange"></date-picker>
+                 </div>`,
+      data() {
+        return {
+          date: '10/10/2016',
+          config: {
+            format: 'DD/MM/YYYY'
+          }
+        }
+      },
+      components: {
+        datePicker: component
+      },
+      methods: {
+        onChange(event) {
+
+        }
+      }
+    });
+
+    let wrapper = new app().$mount();
+    const spy = jest.spyOn(wrapper, 'onChange');
+    wrapper.$data.date = new Date();
+    wrapper.$forceUpdate();
+
+    wrapper.$nextTick(() => {
+      expect(spy).toHaveBeenCalled();
+      spy.mockReset();
+      done()
+    })
+
+  });
+
+});
